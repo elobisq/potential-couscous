fed86b6551c136d838076dccb5029e1a7df5a6e0
elobisq <elobisq@icloud.com>
2024-08-27 16:51:12 +0200

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,5 @@
+// Wait for the entire page to load
+window.addEventListener('load', function() {
+    // Code to run after the page has completely loaded
+    alert('The page has fully loaded!');
+});
